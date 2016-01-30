c42f2a1f08360ff3a533423ff8468ab526671aed
furaga <furaga.fukahori@gmail.com>
2016-01-30 20:00:15 +0900

    recreated

---
+++ InstantScreenCapture/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace InstantScreenCapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openSaveDirectoryButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openSaveDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox1.Location = new System.Drawing.Point(145, 23);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(291, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Capture when pressing PrintScreen";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.openSaveDirectoryButton);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.checkBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.canvas);
+            this.splitContainer1.Size = new System.Drawing.Size(570, 455);
+            this.splitContainer1.SplitterDistance = 151;
+            this.splitContainer1.SplitterWidth = 6;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ＭＳ ゴシック", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Open save directory";
+            // 
+            // openSaveDirectoryButton
+            // 
+            this.openSaveDirectoryButton.Location = new System.Drawing.Point(269, 98);
+            this.openSaveDirectoryButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.openSaveDirectoryButton.Name = "openSaveDirectoryButton";
+            this.openSaveDirectoryButton.Size = new System.Drawing.Size(52, 37);
+            this.openSaveDirectoryButton.TabIndex = 2;
+            this.openSaveDirectoryButton.Text = "../";
+            this.openSaveDirectoryButton.UseVisualStyleBackColor = true;
+            this.openSaveDirectoryButton.Click += new System.EventHandler(this.openSaveDirectoryButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Capture";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(570, 298);
+            this.canvas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openSaveDirectoryToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitFToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(180, 54);
+            // 
+            // openSaveDirectoryToolStripMenuItem
+            // 
+            this.openSaveDirectoryToolStripMenuItem.Name = "openSaveDirectoryToolStripMenuItem";
+            this.openSaveDirectoryToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.openSaveDirectoryToolStripMenuItem.Text = "Open save directory";
+            this.openSaveDirectoryToolStripMenuItem.Click += new System.EventHandler(this.openSaveDirectoryToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(176, 6);
+            // 
+            // exitFToolStripMenuItem
+            // 
+            this.exitFToolStripMenuItem.Name = "exitFToolStripMenuItem";
+            this.exitFToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.exitFToolStripMenuItem.Text = "Exit (&F)";
+            this.exitFToolStripMenuItem.Click += new System.EventHandler(this.exitFToolStripMenuItem_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "InstanceScreenCapture";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 455);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("ＭＳ ゴシック", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Instant Screencapture";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitFToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button openSaveDirectoryButton;
+        private System.Windows.Forms.ToolStripMenuItem openSaveDirectoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ InstantScreenCapture/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace InstantScreenCapture
+{
+    public partial class Form1 : Form
+    {
+        Hooker hooker = new Hooker();
+
+        public string GetRootDir()
+        {
+            string document = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string rootDir = Path.Combine(document, "InstantScreenCpture");
+
+            if (Directory.Exists(rootDir) == false)
+            {
+                Directory.CreateDirectory(rootDir);
+            }
+
+            return rootDir;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hooker.OnKeyHook = OnKeyHook;
+            checkBox1_CheckedChanged(null, null);
+        }
+        
+        private void openRootDirButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                hooker.Hook();
+            }
+            else
+            {
+                hooker.Unhook();
+            }
+        }
+
+        bool OnKeyHook(int code, WM wParam, KBDLLHOOKSTRUCT lParam, Hooker hooker)
+        {
+            if (lParam.vkCode ==  44 && wParam == WM.KEYUP)
+            {
+                takeScreenshot();
+            }
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            takeScreenshot();
+        }
+
+        string getScreenshotSaveDirectoryPath(bool createIfNotExits = false)
+        {
+            string root = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string dirName = "InstantScreenCapture";
+            string dir = System.IO.Path.Combine(root, dirName);
+
+            if (createIfNotExits)
+            {
+                if (System.IO.Directory.Exists(dir) == false)
+                {
+                    System.IO.Directory.CreateDirectory(dir);
+                }
+            }
+
+            return dir;
+        }
+
+        string getScreenshotSaveFilepath()
+        {
+            string dir = getScreenshotSaveDirectoryPath(true);
+
+            var now = DateTime.Now;
+            string filename = string.Format(
+                "{0:0000}-{1:00}-{2:00}-{3:00}-{4:00}-{5:00}.png",
+                now.Year,
+                now.Month,
+                now.Day,
+                now.Hour,
+                now.Minute,
+                now.Second);
+
+            string filepath = System.IO.Path.Combine(dir, filename);
+
+            return filepath;
+        }
+
+        bool isCapturing_ = false;
+
+        void takeScreenshot()
+        {
+            if (isCapturing_)
+            {
+                return;
+            }
+
+            isCapturing_ = true;
+
+            var screenshot = TakeScreenshotInteractive(this, false);
+            if (screenshot == null)
+            {
+                isCapturing_ = false;
+                return;
+            }
+
+            // クリップボードに画像をコピー
+            var clipboardImage = Clipboard.GetImage();
+            if (clipboardImage is Bitmap)
+            {
+                clipboardImage.Dispose();
+            }
+            Clipboard.SetImage(screenshot);
+
+            // 特定のフォルダ内に、画像をpngで保存する
+            screenshot.Save(getScreenshotSaveFilepath());
+
+            // canvasにも画像を表示
+            if (canvas.Image != null)
+            {
+                canvas.Image.Dispose();
+            }
+            canvas.Image = CreateThumbnail(screenshot, canvas.Width, canvas.Height);
+            canvas.Invalidate();
+            isCapturing_ = false;
+        }
+
+        public static Bitmap TakeScreenshotInteractive(Form owner, bool hideOwnerForm = true)
+        {
+            using (var form = new TakeScreenshotForm())
+            {
+                return form.takeScreenshot(owner, hideOwnerForm);
+            }
+        }
+
+        bool closeForce_ = false;
+
+        private void exitFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // プロセスを完全に終了させる
+            closeForce_ = true;
+            Close();
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (closeForce_)
+            {
+                // closeForce_がtrueなら普通にフォームを閉じる
+                return;
+            }
+            else
+            {
+                // さもなくば、ただフォームを隠すだけ（タスクバーに常駐させつづける）
+                if (notifyIcon.Visible)
+                {
+                    e.Cancel = true;
+                    Hide();
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // タスクバーからアイコンを消す
+            notifyIcon.Visible = false;
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            Activate();
+        }
+
+        private void openSaveDirectoryButton_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(getScreenshotSaveDirectoryPath(true));
+        }
+
+        private void openSaveDirectoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(getScreenshotSaveDirectoryPath(true));
+        }
+
+        // サムネイルの作成
+        public Bitmap CreateThumbnail(Bitmap bmp, int w, int h)
+        {
+            Bitmap thumbnail = new Bitmap(w, h, bmp.PixelFormat);
+            using (Graphics g = Graphics.FromImage(thumbnail))
+            {
+                g.Clear(Color.White);
+                SizeF size = GetFittingSize(bmp, w, h);
+                g.DrawImage(bmp, new Rectangle(0, 0, (int)(size.Width), (int)(size.Height)));
+            }
+            return thumbnail;
+        }
+
+        SizeF GetFittingSize(Bitmap bmp, int w, int h)
+        {
+            float ratio = Math.Min((float)w / bmp.Width, (float)h / bmp.Height);
+            float ww = bmp.Width * ratio;
+            float hh = bmp.Height * ratio;
+            return new SizeF(ww, hh);
+        }
+    }
+}
